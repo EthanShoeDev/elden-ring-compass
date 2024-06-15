40a2fcc605a5d75f7acc90e0b1ebc0806a5398f5
Ethan Shumate <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-15 02:50:10 +0000

    stuff

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,3 +7,9 @@ npm completion >> ~/.bashrc
 curl -fsSL https://bun.sh/install | bash # for macOS, Linux, and WSL
 
 . "$HOME/.bashrc"
+
+
+
+mkdir -p ~/.config/husky
+touch ~/.config/husky/init.sh
+echo "source ~/.bashrc" > ~/.config/husky/init.sh
