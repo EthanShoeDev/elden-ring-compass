4bdfce08a9aacbe24b1a2f72993fca6fe36f81f0
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-15 03:02:20 +0000

    chore: Update devcontainer setup script to source .bashrc correctly

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,9 +7,11 @@ npm completion >> ~/.bashrc
 curl -fsSL https://bun.sh/install | bash # for macOS, Linux, and WSL
 
 . "$HOME/.bashrc"
+bun completions >> ~/.bashrc
+. "$HOME/.bashrc"
 
 
 
 mkdir -p ~/.config/husky
 touch ~/.config/husky/init.sh
-echo "source ~/.bashrc" > ~/.config/husky/init.sh
+echo ". $HOME/.bashrc" > ~/.config/husky/init.sh
