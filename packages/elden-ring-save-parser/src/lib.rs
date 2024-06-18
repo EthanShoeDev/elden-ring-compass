9e01a15c58b0031eab3cd2b78bdc61f6dab4456e
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-17 23:54:51 -0400

    chore: Update save parser to serialize SaveSlot struct

--- packages/elden-ring-save-parser/src/lib.rs
+++ packages/elden-ring-save-parser/src/lib.rs
@@ -6,7 +6,9 @@ mod utils;
 mod vm;
 mod write;
 
+use save::common::save_slot::SaveSlot;
 use save::save::save::*;
+use serde::ser::{Serialize, SerializeStruct, Serializer};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -37,10 +39,33 @@ pub fn take_number_slice_by_shared_ref(save_data: &[u8]) -> String {
     }
 }
 
+impl Serialize for SaveSlot {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("SaveSlot", 30)?; // Adjust the number of fields accordingly
+
+        // // Serialize simple fields directly
+        // state.serialize_field("ga_items", &self.ga_items)?;
+        // state.serialize_field("player_game_data", &self.player_game_data)?;
+        // // Example of handling a large array efficiently
+        // // Convert the _0xd0 array to a base64 string for efficient serialization
+        // let _0xd0_base64 = encode(&self._0xd0);
+        // state.serialize_field("_0xd0", &_0xd0_base64)?;
+
+        // // Continue with other fields, applying similar logic
+        // // ...
+
+        state.end()
+    }
+}
+
 #[wasm_bindgen]
-pub fn load_save_contents(save_data: &[u8], slot_index: isize) -> JsValue {
+pub fn load_save_contents(save_data: &[u8], slot_index: usize) -> Result<JsValue, JsValue> {
     let save_result = Save::from_contents(save_data.to_vec());
     let save = save_result.unwrap();
-    save.
-    serde_wasm_bindgen::to_value(&save.save_type)
+    let save_type = save.save_type;
+    let slot = save_type.get_slot(slot_index);
+    Ok(serde_wasm_bindgen::to_value(&slot)?)
 }
