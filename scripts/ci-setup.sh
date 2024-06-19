6251e6706b07bc8019f703a44ac96378808eb139
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-19 13:08:05 -0400

    chore: Update ci-setup.sh to install Rustup and wasm-pack with sudo

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,11 +1,11 @@
 echo "Installing Rustup..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh -s -- -y
 
 # Source the Rust environment immediately to update PATH for the current session
 source "$HOME/.cargo/env"
 
 echo "Installing wasm-pack..."
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sudo sh -s -- -y
 
 
 # Proceed with the rest of the script
