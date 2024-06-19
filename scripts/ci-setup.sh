273adeaaa1f90c82e065cd5632c561ebcef6c51b
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-19 13:03:51 -0400

    chore: Add path to cargo binaries in .bashrc

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,12 +1,16 @@
 echo "Installing Rustup..."
-# Install Rustup (compiler)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Adding binaries to path
 source "$HOME/.cargo/env"
 
 
 echo "Installing wasm-pack..."
-# Install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
+
+echo "Adding binaries to path..."
+echo 'export PATH=$PATH:/vercel/.cargo/bin/' >> "$HOME/.bashrc"
+
+source "$HOME/.bashrc"
 source "$HOME/.cargo/env"
+
