810366427e09fdf4eb450dd9085db3f86bc38473
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-19 12:53:47 -0400

    chore: Rename ci-install.sh to ci-build.sh and update build script

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,5 +1,3 @@
-bun install
-
 echo "Installing Rustup..."
 # Install Rustup (compiler)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -11,3 +9,6 @@ echo "Installing wasm-pack..."
 # Install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
+
+echo "Build static frontend client..."
+bun run build
