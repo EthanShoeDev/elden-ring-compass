46e6bbd7c456462e041c4c93d2081083297b5cf9
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-19 13:06:13 -0400

    chore: Update ci-setup.sh to source Rust environment and build wasm-pack

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,16 +1,12 @@
 echo "Installing Rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-# Adding binaries to path
-source "$HOME/.cargo/env"
 
+# Source the Rust environment immediately to update PATH for the current session
+source "$HOME/.cargo/env"
 
 echo "Installing wasm-pack..."
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
 
-echo "Adding binaries to path..."
-echo 'export PATH=$PATH:/vercel/.cargo/bin/' >> "$HOME/.bashrc"
-
-source "$HOME/.bashrc"
-source "$HOME/.cargo/env"
-
+# Proceed with the rest of the script
+cd packages/elden-ring-save-parser && /vercel/.cargo/bin/wasm-pack build && cd ../..
