44ec5159e5f1b77c9ff931a82ec1d2001fc556dd
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-19 13:10:59 -0400

    chore: Update ci-setup.sh to remove sudo and add cargo binaries to PATH

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,12 +1,10 @@
 echo "Installing Rustup..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-# Source the Rust environment immediately to update PATH for the current session
 source "$HOME/.cargo/env"
 
 echo "Installing wasm-pack..."
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sudo sh -s -- -y
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
-
-# Proceed with the rest of the script
-cd packages/elden-ring-save-parser && /vercel/.cargo/bin/wasm-pack build && cd ../..
+export PATH="$PATH:/vercel/.cargo/bin/"
+# cd packages/elden-ring-save-parser && wasm-pack build && cd ../..
