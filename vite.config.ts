3c121720dd26366da482c277f4eb9c040e6d1398
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-06-27 20:00:26 -0400

    chore: Update ViteImageOptimizer

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     ViteImageOptimizer({
       cache: true,
       cacheLocation: './node_modules/.cache/vite-plugin-image-optimizer',
-      exclude: /^[012345678].*/, // Exclude odd-numbered images
+      exclude: /^[0123456].*/, // Exclude odd-numbered images
     }),
     wasm(),
     comlink(),
