a7376426a002fdf36509e9ed9ef30119fcf80b34
EthanShoeDev <13422990+EthanShoeDev@users.noreply.github.com>
2024-07-28 21:58:49 -0400

    refactor: Add Copy Save as JSON button to Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,17 @@
+import { useSelectedSlot } from '@/stores/slot-selection-store';
+import { Button } from './ui/button';
+import { inventoryDbView } from '@/lib/vm/inventory';
+import { eventsDbView } from '@/lib/vm/events';
+import { regionsDbView } from '@/lib/vm/regions';
+import { statsDbView } from '@/lib/vm/stats';
+import { equipmentDbView } from '@/lib/vm/equipement';
+import { useMutation } from '@tanstack/react-query';
+import Spinner from './ui/spinner';
+import { useState } from 'react';
+
 export function Footer() {
   return (
-    <footer className="flex flex-col items-center pt-10">
+    <footer className="flex flex-col items-center py-10">
       <h3 className="mb-2 text-lg font-bold">Huge thanks to these projects:</h3>
       <ul className="prose list-inside list-disc dark:prose-invert">
         <li>
@@ -43,6 +54,83 @@ export function Footer() {
           Website
         </a>
       </div>
+      <CopySaveAsJsonButton />
     </footer>
+  );
+}
+
+function CopySaveAsJsonButton() {
+  const slot = useSelectedSlot();
+  const [recentSuccess, setRecentSuccess] = useState(false);
+
+  const copyMutation = useMutation({
+    mutationFn: async () => {
+      if (!slot) return;
+
+      const equipmentVm = equipmentDbView(slot);
+      const eventsVm = eventsDbView(slot);
+      const inventoryVm = inventoryDbView(slot);
+      const regionsVm = regionsDbView(slot);
+      const statsVm = statsDbView(slot);
+
+      function uint8ArrayToBase64(uint8Array: Uint8Array) {
+        let binary = '';
+        const len = uint8Array.length;
+        for (let i = 0; i < len; i++) {
+          binary += String.fromCharCode(uint8Array[i]);
+        }
+        return btoa(binary);
+      }
+
+      function trimTrailingZeros(uint8Array: Readonly<Uint8Array>) {
+        let endIndex = uint8Array.length - 1;
+        while (endIndex >= 0 && uint8Array[endIndex] === 0) {
+          endIndex--;
+        }
+        return uint8Array.slice(0, endIndex + 1);
+      }
+
+      const result = {
+        stats: statsVm,
+        regions: regionsVm[0],
+        events: {
+          known_events: eventsVm,
+          event_buffer: uint8ArrayToBase64(
+            trimTrailingZeros(slot.event_flags.flags)
+          ),
+        },
+        inventory: inventoryVm.items,
+        equipment: equipmentVm,
+      };
+
+      await navigator.clipboard.writeText(
+        JSON.stringify(
+          result,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          (_, value) => (typeof value === 'bigint' ? value.toString() : value),
+          2
+        )
+      );
+    },
+    onSuccess: () => {
+      setRecentSuccess(true);
+      setTimeout(() => {
+        setRecentSuccess(false);
+      }, 2000);
+    },
+  });
+
+  return (
+    <Button
+      disabled={!slot || copyMutation.isPending}
+      onClick={() => {
+        copyMutation.mutate();
+      }}
+      className="flex gap-4"
+    >
+      {copyMutation.isPending && <Spinner />}
+      {recentSuccess && <span className="text-green-500">✔</span>}
+      {copyMutation.error ? copyMutation.error.message : 'Copy Save as JSON'}
+    </Button>
   );
 }
